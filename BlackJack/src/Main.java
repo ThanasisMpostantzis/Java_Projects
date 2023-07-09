e3fc910ebce2f468ccd1a0a087fafd185fc95c04
t_mpostantzis <34251516+ThanasisMpostantzis@users.noreply.github.com>
2023-07-09 19:45:22 +0300

    Main.java

--- BlackJack/src/Main.java
+++ BlackJack/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		 
 		System.out.println("Welcome to the simplified BlackJack game !!\n");
 		Player player = new Player();
 		System.out.println("You get a " + player.getCard1() +
